46ed427d54aaa9e393d7cf013f34b07552c755ec
Louis Thievent <louis.thievent@gmail.com>
2019-01-08 12:34:16 +0100

    made location manager work

--- app/src/main/java/fr/wcs/dojolocalisation/MainActivity.java
+++ app/src/main/java/fr/wcs/dojolocalisation/MainActivity.java
@@ -1,9 +1,14 @@
 package fr.wcs.dojolocalisation;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -11,19 +16,25 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.widget.Toast;
 
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
     private LocationManager mLocationManager;
     private Location mLocation;
+    private boolean mapIsLoaded = false;
     public static final int LOCATION_PERMISSION = 1;
+    public static final int UPDATE_TIME = 1000;
+    public static final int UPDATE_DISTANCE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +44,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         if ( ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED
                 || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION);
-
         }
+        else setUpMap();
     }
 
     @Override
@@ -42,15 +53,23 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == LOCATION_PERMISSION) {
             if (grantResults.length == 2 && grantResults[1] == PackageManager.PERMISSION_GRANTED && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                        .findFragmentById(R.id.map);
-                mapFragment.getMapAsync(this);
+                setUpMap();
             }
             else {
-                Toast.makeText(this, "You must accept permission", Toast.LENGTH_SHORT).show();
+                finish();
             }
         }
     }
+
+    @SuppressLint("MissingPermission")
+    public void setUpMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, UPDATE_TIME, UPDATE_DISTANCE, this);
+    }
+
 
     /**
      * Manipulates the map once available.
@@ -64,10 +83,40 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mapIsLoaded = true;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        // Add a marker in Sydney and move the camera
+//        LatLng userPos = new LatLng(mLocation.getLatitude(), mLocation.getLongitude());
+//        mMap.addMarker(new MarkerOptions().position(userPos).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(userPos));
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLocation = location;
+        Toast.makeText(this, "updated", Toast.LENGTH_SHORT).show();
+        if (mapIsLoaded){
+            runTasks(location);
+        }
+    }
+
+    private void runTasks(Location location) {
+        LatLng userPos = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(userPos).title("Hello"));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
